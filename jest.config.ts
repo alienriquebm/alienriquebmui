1a0b31733f695cf2306582eeef679ffc6bb782ce
Ali Enrique Briceño <alienriquebm@gmail.com>
2024-09-30 12:17:06 -0400

    installed jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  clearMocks: true,
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
+
+export default config;
