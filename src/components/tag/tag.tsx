11414b12d941fad4e668050c13b54323f26394e2
Luis Suarez <lsuarezweb@gmail.com>
2024-10-01 15:04:54 -0400

    refactor(Tag): change default color and spacing

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -3,7 +3,7 @@ import { ITag } from './tag.interfaces';
 
 export default function Tag({ children, className, dataCy, leftIcon, rightIcon }: ITag) {
 	const tagClasses = cn(
-		'text-sm border border-green-600 px-8 py-2 rounded-lg flex items-center gap-2',
+		'text-sm border border-slate-500 px-4 py-2 rounded-lg flex items-center gap-2',
 		className
 	);
 
