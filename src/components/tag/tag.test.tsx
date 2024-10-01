0eef77f0ed32150f80a829000c20ea62342e0196
Luis Suarez <lsuarezweb@gmail.com>
2024-10-01 15:13:10 -0400

    fix(tag.test): fix icons test in component

--- src/components/tag/tag.test.tsx
+++ src/components/tag/tag.test.tsx
@@ -1,49 +1,48 @@
 import { render } from '@testing-library/react';
 import Tag from './tag';
-import { MdSearch } from 'react-icons/md';
 
 const tagRender = ({
-  leftIcon,
-  rightIcon,
+	leftIcon,
+	rightIcon,
 }: { leftIcon?: React.ReactNode; rightIcon?: React.ReactNode } = {}) =>
-  render(
-    <Tag leftIcon={leftIcon} rightIcon={rightIcon}>
-      Tag
-    </Tag>,
-  );
+	render(
+		<Tag leftIcon={leftIcon} rightIcon={rightIcon}>
+			Tag
+		</Tag>
+	);
 
 describe('Tag', () => {
-  it('should render the Tag component', () => {
-    const { container } = tagRender();
-    expect(container).toBeInTheDocument();
-  });
+	it('should render the Tag component', () => {
+		const { container } = tagRender();
+		expect(container).toBeInTheDocument();
+	});
 
-  it('should render its children', () => {
-    const { getByText } = tagRender();
+	it('should render its children', () => {
+		const { getByText } = tagRender();
 
-    expect(getByText(/Tag/i)).toBeInTheDocument();
-  });
+		expect(getByText(/Tag/i)).toBeInTheDocument();
+	});
 
-  it('should render left icon when is present', () => {
-    const { container } = tagRender({ leftIcon: <MdSearch /> });
-    const leftIcon = container.querySelector('svg');
+	it('should render left icon when is present', () => {
+		const { container } = tagRender({ leftIcon: '🔎' });
+		const leftIcon = container.querySelector('span');
 
-    expect(leftIcon).toBeInTheDocument();
-  });
+		expect(leftIcon).toBeInTheDocument();
+	});
 
-  it('should render right icon when is present', () => {
-    const { container } = tagRender({ rightIcon: <MdSearch /> });
-    const rightIcon = container.querySelector('svg');
+	it('should render right icon when is present', () => {
+		const { container } = tagRender({ rightIcon: '🔎' });
+		const rightIcon = container.querySelector('span');
 
-    expect(rightIcon).toBeInTheDocument();
-  });
+		expect(rightIcon).toBeInTheDocument();
+	});
 
-  it('should render left and right icons when are present', () => {
-    const { container } = tagRender({ leftIcon: <MdSearch />, rightIcon: <MdSearch /> });
-    const leftIcon = container.querySelector('svg');
-    const rightIcon = container.querySelectorAll('svg')[1];
+	it('should render left and right icons when are present', () => {
+		const { container } = tagRender({ leftIcon: '🔎', rightIcon: '🔎' });
+		const leftIcon = container.querySelector('span');
+		const rightIcon = container.querySelectorAll('span')[1];
 
-    expect(leftIcon).toBeInTheDocument();
-    expect(rightIcon).toBeInTheDocument();
-  });
+		expect(leftIcon).toBeInTheDocument();
+		expect(rightIcon).toBeInTheDocument();
+	});
 });
