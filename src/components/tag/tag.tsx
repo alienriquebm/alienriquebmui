4eac727a6d537a0141bdc21ad8e434d82a28fba6
Ali Enrique Briceño <alienriquebm@gmail.com>
2024-10-02 14:12:59 -0400

    Refactor tag component to include width fitting

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -3,7 +3,7 @@ import { ITag } from './tag.interfaces';
 
 export function Tag({ children, className, dataCy, leftIcon, rightIcon }: ITag) {
   const tagClasses = cn(
-    'text-sm !border border-slate-500 px-4 py-2 rounded-lg flex items-center gap-2',
+    'text-sm border border-slate-500 px-4 py-2 rounded-lg flex items-center gap-2 w-fit',
     className,
   );
   return (
