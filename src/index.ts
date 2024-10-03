d9c1a2eb7ecb40afadba34336d72211b9f6c7379
Jorge Castillo <jj.jorgecastillo@gmail.com>
2024-10-03 12:20:39 -0500

    feat(switch): add component switch with test and histories

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './styles.css';
-export { Tag } from './components/tag';
+
 export { Avatar, AvatarSize } from './components/avatar';
 export { Button } from './components/button';
-export { Textarea } from './components/textarea';
 export { Switch } from './components/switch';
+export { Tag } from './components/tag';
+export { Textarea } from './components/textarea';
