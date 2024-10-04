7b99b15fe8b74a2ffa331a6ed3b05791596628ea
Jorge Castillo <jj.jorgecastillo@gmail.com>
2024-10-04 12:15:07 -0500

    feat(select): added select and select item

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { Tag } from './components/tag';
 export { Avatar, AvatarSize } from './components/avatar';
 export { Button } from './components/button';
 export { Textarea } from './components/textarea';
+export { Select } from './components/select';
+export { SelectItem } from './components/select/select-item';
