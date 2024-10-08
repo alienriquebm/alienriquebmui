3eb366bbd58da0afbf2a4d81e054839333fb107b
Jorge Castillo <jj.jorgecastillo@gmail.com>
2024-10-07 23:46:36 -0500

    feat(table): export table and usericon

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './styles.css';
 
 export { InfoIcon } from './icons';
+export { UserIcon } from './icons';
 
 export { Avatar, AvatarSize } from './components/avatar';
 export { Button } from './components/button';
@@ -11,6 +12,7 @@ export { Select } from './components/select';
 export { SelectItem } from './components/select/select-item';
 export { Spinner } from './components/spinner';
 export { Switch } from './components/switch';
+export { Table } from './components/table';
 export { Tag } from './components/tag';
 export { Textarea } from './components/textarea';
 export { Tooltip } from './components/tooltip';
