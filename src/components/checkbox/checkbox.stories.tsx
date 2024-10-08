b1637eda5af549cba264abda154fde118ecef1b8
Ali Enrique Briceño <alienriquebm@gmail.com>
2024-10-08 09:31:00 -0400

    Refactor checkbox stories title

--- src/components/checkbox/checkbox.stories.tsx
+++ src/components/checkbox/checkbox.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Checkbox } from './checkbox';
 
 const meta: Meta<typeof Checkbox> = {
-  title: 'Design System/Checkbox NEW',
+  title: 'Design System/Checkbox',
   component: Checkbox,
   parameters: {
     layout: 'centered',
